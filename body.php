3002bb828ec1f72d2e22f7b0838f711d1354dba8
NishanthStriker <nishanthsamrajj@gmail.com>
2020-03-13 17:30:00 +0530

    first

---
+++ body.php
@@ -0,0 +1,11 @@
+<div class="body">
+<div class="img">
+<img src="Penguins.jpg" height="500px" width="1000px"/>
+</div>
+<div class="content">
+<p>W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 1999-2020 by Refsnes Data. All Rights Reserved.
+Powered by W3.CSS.
+</p>
+
+</div>
+</div>
